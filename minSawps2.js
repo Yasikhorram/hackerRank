301af71ebbda876771e6852fc9164767dc2a0401
Yasikhorram <khorramyasaman@gmail.com>
2022-04-15 12:22:46 -0500

    correction

--- minSawps2.js
+++ minSawps2.js
@@ -8,11 +8,17 @@ function minimumSwaps(arr) {
   let swap = 0;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] !== i + 1) {
-      let temp = arr[i];
-      arr[i] = arr[temp - 1];
-      arr[temp - 1] = temp;
-      swap++;
+      console.log("arr[i]", arr[i]);
+      while (arr[i] !== i + 1) {
+        let temp = arr[i];
+        arr[i] = arr[temp - 1];
+        arr[temp - 1] = temp;
+        swap++;
+      }
     }
   }
+
   return swap;
 }
+
+console.log(minimumSwaps([4, 3, 1, 2]));
