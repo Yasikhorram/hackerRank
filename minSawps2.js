b93b31b76dec854e3302da64bb7e72b39100a51d
Yasikhorram <khorramyasaman@gmail.com>
2022-04-14 23:53:59 -0500

    write a basic loop with swap

---
+++ minSawps2.js
@@ -0,0 +1,14 @@
+/*
+-unordered arr - without duplicates - allowed to swap 2 elements
+-find min num of swap
+
+*/
+
+function minimumSwaps(arr) {
+  let swap = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] - 1 !== i) {
+    }
+  }
+  return swap;
+}
