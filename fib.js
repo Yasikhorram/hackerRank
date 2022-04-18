dbc530beaadb162dac8443b37b0be83aed30bd98
Yasikhorram <khorramyasaman@gmail.com>
2022-04-17 21:46:09 -0500

    solved

--- fib.js
+++ fib.js
@@ -4,4 +4,9 @@ fib(1) = 1
 fib(n) = fib(n-1) + fib(n-2)
 
 */
-function fibonacci(n) {}
+function fibonacci(n) {
+  if (n < 3) {
+    return 1;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
