9a9b36e148ea4891b298bbfc20020eb3bbdf5050
Yasikhorram <khorramyasaman@gmail.com>
2022-04-19 17:20:28 -0500

    solved

--- factorial.js
+++ factorial.js
@@ -4,4 +4,12 @@ factorial 4 -> 4! -> 4*3*2*1 = 24
 factorial 0 -> 1
 */
 
-function factorial(n) {}
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(4));
