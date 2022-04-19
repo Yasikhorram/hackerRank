ad45a6fd940fbd9dc8e52a731c982ca6e35ca8cb
Yasikhorram <khorramyasaman@gmail.com>
2022-04-19 17:07:03 -0500

    skeleton of factorial

--- davisStairCase.js
+++ davisStairCase.js
@@ -2,4 +2,7 @@
 -climb each staircase 1, 2, or 3steps at a time.
 - return the number of ways Davis can climb the staircase, modulo 10000000007
 */
-function stepPerms(n) {}
+function stepPerms(n) {
+  let possibilities = 0;
+  return possibilities;
+}
---
+++ factorial.js
@@ -0,0 +1,7 @@
+/*
+a Factorial is a product of an integer and ll integers below
+factorial 4 -> 4! -> 4*3*2*1 = 24
+factorial 0 -> 1
+*/
+
+function factorial(n) {}
