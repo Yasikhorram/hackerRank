8ae9da8106b5358a60b7d42382a977945ccde5d1
Yasikhorram <khorramyasaman@gmail.com>
2022-04-19 22:43:20 -0500

    solved

--- frequencyCounter.js
+++ frequencyCounter.js
@@ -7,17 +7,23 @@
 
 function squaredSame(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
-  let allSquared = false;
   let obj1 = {};
   let obj2 = {};
-  for (var i = 0; i < arr1.length; i++) {
+  for (let i = 0; i < arr1.length; i++) {
     obj1[arr1[i]] = obj1[arr1[i]] + 1 || 1;
   }
   for (let val of arr2) {
     obj2[val] = obj2[val] + 1 || 1;
   }
-  console.log(obj2);
+  for (let key in obj1) {
+    if (!(key ** 2 in obj2)) {
+      return false;
+    }
+    if (obj1[key] !== obj2[key ** 2]) {
+      return false;
+    }
+  }
 
-  return allSquared;
+  return true;
 }
 console.log(squaredSame([1, 2, 3, 1], [4, 1, 1, 9]));
