cfa1ee30f9f9554e18760f4cdcb583749664bfc2
Yasikhorram <khorramyasaman@gmail.com>
2022-04-19 21:31:53 -0500

    made 2 frequencycounter objects

--- frequencyCounter.js
+++ frequencyCounter.js
@@ -8,8 +8,16 @@
 function squaredSame(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   let allSquared = false;
-  let obj = {};
-  for (var i = 0; i < arr1.length; i++) {}
+  let obj1 = {};
+  let obj2 = {};
+  for (var i = 0; i < arr1.length; i++) {
+    obj1[arr1[i]] = obj1[arr1[i]] + 1 || 1;
+  }
+  for (let val of arr2) {
+    obj2[val] = obj2[val] + 1 || 1;
+  }
+  console.log(obj2);
 
   return allSquared;
 }
+console.log(squaredSame([1, 2, 3, 1], [4, 1, 1, 9]));
