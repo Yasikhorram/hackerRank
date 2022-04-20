7327be8a06ae3ffc6429fc19d20037d4233e8391
Yasikhorram <khorramyasaman@gmail.com>
2022-04-19 21:11:35 -0500

    skeleton of frequency counter

---
+++ frequencyCounter.js
@@ -0,0 +1,15 @@
+/*
+- is the second arr squared of first arr
+- order doesnt matter but the frequency should be the same
+- [1,2,3] -> [4,1,9] -> true
+-[1,2,1] -> [4,1] -> false must be the same frequency
+*/
+
+function squaredSame(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  let allSquared = false;
+  let obj = {};
+  for (var i = 0; i < arr1.length; i++) {}
+
+  return allSquared;
+}
