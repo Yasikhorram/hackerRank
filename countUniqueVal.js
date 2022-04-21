ec9d75148d5a16afbf7e18ad834f4e9f819aa0ad
Yasikhorram <khorramyasaman@gmail.com>
2022-04-20 22:51:35 -0500

    solved

--- countUniqueVal.js
+++ countUniqueVal.js
@@ -3,12 +3,14 @@
 return int which is numbers of unique values
 */
 
-function countUniqueValues(arr) {
+function countUniqueVal(arr) {
   let count = 0;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== ar[i + 1]) {
+    if (arr[i] !== arr[i + 1]) {
       count++;
     }
   }
   return count;
 }
+
+console.log(countUniqueVal([-2, -1, -1, 0, 1]));
