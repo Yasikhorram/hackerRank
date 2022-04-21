7b4ce5ca8885c7467c0adfb5f921559e852e057d
Yasikhorram <khorramyasaman@gmail.com>
2022-04-20 22:00:37 -0500

    two pointers skeleton

---
+++ sumZero.js
@@ -0,0 +1,12 @@
+/*
+-function accepts sorted arr
+- function returns the first pair which sum up to 0 in an arr or return undefined
+*/
+
+function sumZero(arr) {
+  if (arr[0] >= 0) {
+    return undefined;
+  }
+  let pointer1;
+  let pointer2;
+}
