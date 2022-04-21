7a1a47075976d3a50f0192942b319389ea948cb9
Yasikhorram <khorramyasaman@gmail.com>
2022-04-20 20:44:46 -0500

    solved

--- anagram.js
+++ anagram.js
@@ -1,4 +1,26 @@
 /*
 Anagrams are words same letters with same frequencies
 */
-function validAnagram(str1, str2) {}
+function validAnagram(str1, str2) {
+  let arr1 = str1.split("");
+  let arr2 = str2.split("");
+  let frequency1 = {};
+  let frequency2 = {};
+  for (let item of arr1) {
+    frequency1[item] = frequency1[item] + 1 || 1;
+  }
+  for (let item of arr2) {
+    frequency2[item] = frequency2[item] + 1 || 1;
+  }
+  for (let key in frequency1) {
+    if (!(key in frequency2)) {
+      return false;
+    }
+    if (frequency1[key] !== frequency2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(validAnagram("aaz", "zza"));
