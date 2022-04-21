8bccaf2108fa016c2a236eea688e9b58e9800d2a
Yasikhorram <khorramyasaman@gmail.com>
2022-04-20 22:24:07 -0500

    solved

--- davisStairCase.js
+++ davisStairCase.js
@@ -3,6 +3,12 @@
 - return the number of ways Davis can climb the staircase, modulo 10000000007
 */
 function stepPerms(n) {
-  let possibilities = 0;
-  return possibilities;
+  if (n === 1) {
+    return 1;
+  } else if (n === 2) {
+    return 2;
+  } else if (n === 3) {
+    return 4;
+  }
+  return stepPerms(n - 1) + stepPerms(n - 2) + stepPerms(n - 3);
 }
--- sumZero.js
+++ sumZero.js
@@ -7,6 +7,18 @@ function sumZero(arr) {
   if (arr[0] >= 0) {
     return undefined;
   }
-  let pointer1;
-  let pointer2;
+
+  let pointer1 = 0;
+  let pointer2 = arr.length - 1;
+  while (pointer1 < pointer2) {
+    let sum = arr[pointer1] + arr[pointer2];
+    if (sum === 0) {
+      return [arr[pointer1], arr[pointer2]];
+    } else if (sum > 0) {
+      pointer2--;
+    } else {
+      pointer1++;
+    }
+  }
 }
+console.log(sumZero([-1, 1, 2, 3]));
