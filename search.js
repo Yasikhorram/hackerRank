df12bf72aef4b9c3f77e2cd289bedf990e2dc32c
Yasikhorram <khorramyasaman@gmail.com>
2022-04-24 17:45:41 -0500

    solved

--- search.js
+++ search.js
@@ -4,6 +4,19 @@
 */
 function search(arr, n) {
   let min = 0;
-  let max = Math.floor((arr.length - 1) / 2);
+  let max = arr.length - 1;
+
+  while (min <= max) {
+    let mid = Math.floor((min + max) / 2);
+
+    if (n < arr[mid]) {
+      max = mid - 1;
+    } else if (n > arr[mid]) {
+      min = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+  return -1;
 }
 console.log([1, 3, 4, 6, 7, 10, 13], 10);
