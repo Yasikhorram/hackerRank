faee1a14cecba71bf29c819663ad55ab7b0c9464
Yasikhorram <khorramyasaman@gmail.com>
2022-04-23 22:09:57 -0500

    take care of edge case

--- maxSubArrSum.js
+++ maxSubArrSum.js
@@ -1,9 +1,13 @@
 /*
 - accepts an arr of int and a number
 - return the max sum of n cosecutive elements
+- avoid O(N**2)
 */
 
 function maxSubArrSum(arr, n) {
+  if (n > arr.length) {
+    return null;
+  }
   let maxSum = 0;
   return maxSum;
 }
