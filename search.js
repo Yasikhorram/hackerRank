6998445430a28d04f922a47c7e12a6eb854a6ab2
Yasikhorram <khorramyasaman@gmail.com>
2022-04-24 10:20:41 -0500

    skeleton of divide and conquer

---
+++ search.js
@@ -0,0 +1,9 @@
+/*
+- Using divide and conquer algo -> o(log n)
+
+*/
+function search(arr, n) {
+  let min = 0;
+  let max = Math.floor((arr.length - 1) / 2);
+}
+console.log([1, 3, 4, 6, 7, 10, 13], 10);
