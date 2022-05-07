36900bb0bc376d29c041ff55f45b37b79e3b1ec5
Yasikhorram <khorramyasaman@gmail.com>
2022-05-07 14:52:03 -0500

    codewars dont give 5 skeleton

---
+++ dontGiveFive.js
@@ -0,0 +1,15 @@
+/*
+Don't give me five!
+In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+
+*/
+function dontGiveMeFive(start, end) {
+  let arr = [];
+  for (let i = start; i <= end; i++) {
+    if (i.toString().indexOf("5") > -1) {
+      arr.push(i);
+    }
+  }
+  return arr.length;
+}
+console.log(dontGiveMeFive());
