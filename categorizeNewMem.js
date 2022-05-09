56c0bcaf57b002affb06393a3648387c9c63034e
Yasikhorram <khorramyasaman@gmail.com>
2022-05-08 20:55:06 -0500

    solved

--- categorizeNewMem.js
+++ categorizeNewMem.js
@@ -2,6 +2,11 @@
 https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa/train/javascript
 */
 function openOrSenior(data) {
-  let results = [];
-  return results;
+  return data.map((innerArr) => {
+    if (innerArr[0] >= 55 && innerArr[1] > 7) {
+      return "Senior";
+    } else {
+      return "Open";
+    }
+  });
 }
