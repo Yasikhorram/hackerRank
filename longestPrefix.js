895ac1615933638e3c7afc62aa9a892aa3faa13a
Yasikhorram <khorramyasaman@gmail.com>
2022-06-10 23:48:48 -0500

    solved

--- longestPrefix.js
+++ longestPrefix.js
@@ -4,4 +4,15 @@ Write a function to find the longest common prefix string amongst an array of st
 If there is no common prefix, return an empty string "".
 */
 
-var longestCommonPrefix = function (strs) {};
+var longestCommonPrefix = function (strs) {
+  strs.sort();
+  console.log("strs", strs);
+  for (let i = 0; i < strs[0].length; i++) {
+    if (strs[0][i] !== strs[strs.length - 1][i]) {
+      return strs[0].substr(0, i);
+    }
+  }
+  return strs[0];
+};
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
