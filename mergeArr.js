a159b07baed6dac1372d6e56b01174a08d8bac18
Yasikhorram <khorramyasaman@gmail.com>
2022-06-13 23:22:05 -0500

    add class of Node

--- mergeArr.js
+++ mergeArr.js
@@ -3,5 +3,13 @@ You are given the heads of two sorted linked lists list1 and list2.
 
 Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
 */
+// class Node
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
 
-var mergeTwoLists = function (list1, list2) {};
+var mergeTwoLists = function (list1, list2) {
+  let output = [];
+  return output;
+};
