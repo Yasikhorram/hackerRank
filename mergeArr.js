699de08a8df5013bef643d827fc8152f2e2c072b
Yasikhorram <khorramyasaman@gmail.com>
2022-06-13 23:25:22 -0500

    add class of SinglyLinkedList

--- mergeArr.js
+++ mergeArr.js
@@ -4,11 +4,20 @@ You are given the heads of two sorted linked lists list1 and list2.
 Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
 */
 // class Node
-function ListNode(val) {
-  this.val = val;
-  this.next = null;
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
+class SingyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
 var mergeTwoLists = function (list1, list2) {
   let output = [];
   return output;
