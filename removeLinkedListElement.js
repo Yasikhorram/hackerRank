62365f2e8bec84ef927bb45018b2eb7a9c0d9969
Yasikhorram <khorramyasaman@gmail.com>
2022-06-23 19:35:58 -0500

    skeleton

---
+++ removeLinkedListElement.js
@@ -0,0 +1,7 @@
+/*
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+*/
+
+var removeElements = function (head, val) {};
