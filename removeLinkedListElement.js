ba0ebe3619434a5fb0dd3b7912b1b9a46138be62
Yasikhorram <khorramyasaman@gmail.com>
2022-06-26 23:30:23 -0500

    solved

--- removeLinkedListElement.js
+++ removeLinkedListElement.js
@@ -1,5 +1,9 @@
 /*
 Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
 Input: head = [1,2,6,3,4,5,6], val = 6
 Output: [1,2,3,4,5]
 */
@@ -7,5 +11,13 @@ Output: [1,2,3,4,5]
 var removeElements = function (head, val) {
   let newNode = new listNode();
   let current = newNode;
-  while (head) {}
+  while (head) {
+    if (head.val !== val) {
+      current.next = head;
+      current = current.next;
+    }
+    head = head.next;
+  }
+  current.next = null;
+  return newNode.next;
 };
